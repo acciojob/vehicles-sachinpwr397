a46b3bda3de8d9fad63f8a0154f8099d1c0a19ee
sachinpwr397 <79899346+sachinpwr397@users.noreply.github.com>
2024-04-06 13:17:44 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,9 +2,23 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        F1 mercedes = new F1("mercedes", false);
+        // Create an instance of F1
+        F1 mercedes = new F1("Mercedes", false);
+
+        // Move the F1 car
         mercedes.move(40, 0);
+
+        // Accelerate the F1 car
         mercedes.accelerate(20);
+
+        // Decelerate the F1 car
         mercedes.accelerate(-60);
+
+        // Create an instance of Boat
+        Boat boat = new Boat("Speedy", 10);
+
+        // Display boat information
+        System.out.println("Boat name: " + boat.getVehicleName());
+        System.out.println("Boat capacity: " + boat.getVehicleCapacity());
     }
 }
